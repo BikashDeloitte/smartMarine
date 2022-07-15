38702502bec4a5f6b685ce4c3df80fde57c88698
Kushwaha, Bikash <bkushwaha@deloitte.com>
2022-07-15 11:07:23 +0530

    added ship data

--- src/main/java/com/example/smart_marine_API/smart_marine/SmartMarineApplication.java
+++ src/main/java/com/example/smart_marine_API/smart_marine/SmartMarineApplication.java
@@ -14,36 +14,52 @@ import java.util.Arrays;
 @SpringBootApplication
 public class SmartMarineApplication implements CommandLineRunner {
 
-	@Autowired
-	PortsRepository portsRepository;
-	@Autowired
-	ShipRepository shipRepository;
+    @Autowired
+    PortsRepository portsRepository;
+    @Autowired
+    ShipRepository shipRepository;
 
-	public static void main(String[] args) {
-		SpringApplication.run(SmartMarineApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SmartMarineApplication.class, args);
+    }
 
-	@Override
-	public void run(String... args) throws Exception {
-		Ports port_of_fernandina = new Ports("Port Of Fernandina", 30.6765, -81.4625);
-		Ports port_of_seattle = new Ports("port of seattle", 47.6138, -122.3543);
-		portsRepository.saveAll(Arrays.asList(
-				port_of_fernandina,
-				new Ports("Red Hook Terminal", 40.6840, -74.0062),
-				port_of_seattle,
-				new Ports("port of san francisco", 37.7749, -122.4194),
-				new Ports("port Hueneme", 34.1492, -119.2087),
-				new Ports("Port of San Diego", 32.7355, -117.1772),
-				new Ports("Port of Oakland", 37.7957, -122.2792),
-				new Ports("Port of Los Angeles", 33.7292, -118.2620),
-				new Ports("Port of West Sacramento", 38.5654, -121.5488),
-				new Ports("port Iwaki",37.0490, 140.9693),
-				new Ports("port Shanghai" , 31.2285, 121.4837),
-				new Ports("port Taipei", 25.1022, 121.6341),
-				new Ports("port Brisbane", -26.6409, 152.6192)
-		));
+    @Override
+    public void run(String... args) throws Exception {
+        Ports portFernandina = new Ports("Port Of Fernandina", 30.6765, -81.4625);
+        Ports portSeattle = new Ports("port of seattle", 47.6138, -122.3543);
+        Ports redHookTerminal = new Ports("Red Hook Terminal", 40.6840, -74.0062);
+        Ports portOfSanFrancisco = new Ports("port of san francisco", 37.7749, -122.4194);
+        Ports portHueneme = new Ports("port Hueneme", 34.1492, -119.2087);
+        Ports portOfSanDiego = new Ports("Port of San Diego", 32.7355, -117.1772);
+        Ports portOfOakland = new Ports("Port of Oakland", 37.7957, -122.2792);
+        Ports portOfLosAngeles = new Ports("Port of Los Angeles", 33.7292, -118.2620);
+        Ports portOfWestSacramento = new Ports("Port of West Sacramento", 38.5654, -121.5488);
+        Ports portIwaki = new Ports("port Iwaki", 37.0490, 140.9693);
+        Ports portShanghai = new Ports("port Shanghai", 31.2285, 121.4837);
+        Ports portTaipei = new Ports("port Taipei", 25.1022, 121.6341);
+        Ports portBrisbane = new Ports("port Brisbane", -26.6409, 152.6192);
+        portsRepository.saveAll(Arrays.asList(
+                portFernandina,
+                redHookTerminal,
+                portSeattle,
+                portOfSanFrancisco,
+                portHueneme,
+                portOfSanDiego,
+                portOfOakland,
+                portOfLosAngeles,
+                portOfWestSacramento,
+                portIwaki,
+                portShanghai,
+                portTaipei,
+                portBrisbane
+        ));
 
+        shipRepository.saveAll(Arrays.asList(
+                new Ship(1L, portSeattle, portIwaki, 40.9799, -175.2444),
+                new Ship(2L, portOfSanFrancisco, portShanghai, 27.3718, 175.6179),
+                new Ship(3L, portTaipei, portOfOakland, 22.5937, 164.0201),
+                new Ship(4L, portBrisbane, portOfLosAngeles, -00.7031, -170.3242)
+        ));
 
-
-	}
+    }
 }
